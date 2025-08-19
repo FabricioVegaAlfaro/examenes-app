5415284ee68bcca10d21efde9e936a46d1519c01
FabricioVegaAlfaro <fabriciovega0123@gmail.com>
2025-08-19 17:58:39 -0600

    Cambios en el token 2

--- src/utils/tokenCode.ts
+++ src/utils/tokenCode.ts
@@ -1,4 +1,4 @@
-export function generateTokenCode(length = 8) {
+export function generateTokenCode(length = 5) {
   const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789'; // sin confusos 0O1I
   let out = '';
   for (let i = 0; i < length; i++) {
